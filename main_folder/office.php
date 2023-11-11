a53b73c023fb27afab796f916b497cc37ba7127e
cedrick chu <cedrickchu123@gmail.com>
2023-11-11 21:10:56 +0800

    remove footer-item

--- main_folder/office.php
+++ main_folder/office.php
@@ -160,29 +160,7 @@
             </div>
             <footer class="footer">
                 <div class="container-fluid">
-                    <nav>
-                        <ul class="footer-menu">
-                            <li>
-                                <a href="#">
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Company
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Portfolio
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Blog
-                                </a>
-                            </li>
-                        </ul>
+                    <nav> 
                         <p class="copyright text-center">
                             ©
                             <script>
